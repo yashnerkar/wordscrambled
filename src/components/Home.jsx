3a86fd4294c030c6d84174b7233f1831a6f2b49f
yash-png <yashnerkar512@gmail.com>
2022-09-15 02:30:18 +0530

    init commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="border border-dark">
+            <div className="container">
+                <h1>Arrange all the technical words and make a passcode by taking last letter of words whose index is prime which is less than 15!!!</h1>
+                <div className="row">
+                    <div classNam="col-md-3">
+                        <img src="images/1.png" alt="" />
+                    </div>
+                    <div classNam="col-md-3">
+                        <img src="images/2.png" alt="" />
+                    </div>
+                    <div classNam="col-md-3">
+                        <img src="images/3.png" alt="" />
+                    </div>
+                    <div classNam="col-md-3">
+                        <img src="images/4.png" alt="" />
+                    </div>
+                </div>
+                {/* second row */}
+                <div className="row">
+                    <div classNam="col-md-3">
+                        <img src="images/5.png" alt="" />
+                    </div>
+                    <div classNam="col-md-3">
+                        <img src="images/6.png" alt="" />
+                    </div>
+                    <div classNam="col-md-3">
+                        <img src="images/7.png" alt="" />
+                    </div>
+                    <div classNam="col-md-3">
+                        <img src="images/8.png" alt="" />
+                    </div>
+                </div>
+                {/* third row */}
+                <div className="row">
+                    <div classNam="col-md-3">
+                        <img src="images/9.png" alt="" />
+                    </div>
+                    <div classNam="col-md-3">
+                        <img src="images/10.png" alt="" />
+                    </div>
+                    <div classNam="col-md-3">
+                        <img src="images/11.png" alt="" />
+                    </div>
+                    <div classNam="col-md-3">
+                        <img src="images/12.png" alt="" />
+                    </div>
+                </div>
+                {/* fourth row */}
+                <div className="row">
+                    <div classNam="col-md-3">
+                        <img src="images/13.png" alt="" />
+                    </div>
+                    <div classNam="col-md-3">
+                        <img src="images/14.png" alt="" />
+                    </div>
+                    <div classNam="col-md-3">
+                        <img src="images/15.png" alt="" />
+                    </div>
+                    <div classNam="col-md-3">
+                        <img src="images/16.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
